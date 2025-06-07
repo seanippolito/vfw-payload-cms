fae3433ffceabef06dd4ca7d0e730bcfc05f792b
Sean Ippolito <ippolitosean@gmail.com>
2025-06-06 19:29:25 -0700

    add migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250607_022603 from './20250607_022603';
+
+export const migrations = [
+  {
+    up: migration_20250607_022603.up,
+    down: migration_20250607_022603.down,
+    name: '20250607_022603'
+  },
+];
